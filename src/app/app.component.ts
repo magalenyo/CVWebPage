460e1c1e3b02b4547b47bca2effd9920a3814f46
magalenyo <magalenyo7@gmail.com>
2021-12-24 02:09:59 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { faCalendarAlt, faEnvelope, faMapMarkerAlt, faPhoneAlt } from '@fortawes
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss', './styles.scss'],
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
