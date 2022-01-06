15ab92d28cb1573bee55b5551a5532c0f00804ca
magalenyo <magalenyo7@gmail.com>
2022-01-06 18:12:23 +0100

    Add zoom on click

--- src/app/components/carousel-boundead/carousel-boundead.component.ts
+++ src/app/components/carousel-boundead/carousel-boundead.component.ts
@@ -8,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselBoundeadComponent implements OnInit {
 
   readonly defaultDelay = 5000;
+  readonly defaultClass = '.slider__img';
+
+  modal;
+  modalImg;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  ngAfterViewInit() : void {
+
+    this.modal = document.getElementById("carousel-modal");
+    this.modalImg = document.getElementById("img01");
+
+    $(this.defaultClass).on('click', (e) => {
+        const thisObject = e.currentTarget;
+
+        this.modalImg.src = (<HTMLImageElement> thisObject).src;
+
+        this.openModal();
+      });
+
+
+    const span : HTMLElement = document.getElementsByClassName("close")[0] as HTMLElement;
+
+    span.onclick = () => { 
+      this.closeModal();
+    }
+
+    $('html').click((e) => {
+      if($(e.target).hasClass('modal')) {
+          this.closeModal();
+      } 
+    });
+  }
+
+  openModal() {
+    this.modal.style.display = "flex";
+
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeModal() {
+    this.modal.style.display = "none";
+
+    document.body.style.overflow = 'scroll';
   }
 
 }
