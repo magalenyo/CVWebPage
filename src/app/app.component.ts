e3929cbe756cda830a3a307ccc8e2f6cddb17689
magalenyo <magalenyo7@gmail.com>
2022-02-10 01:14:49 +0100

    Revert "Add Google Analytics"
    This reverts commit 0a04bb33a63ca29a40e499fa687b08c1042b6d5f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { style } from '@angular/animations';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
-
 
 import * as $ from "jquery";
 
-// tslint:disable-next-line:ban-types
-declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -19,24 +14,8 @@ export class AppComponent implements OnInit {
 
   title = 'Miguel Ángel Bueno Rivera';
 
-  constructor(private router: Router) {
-    
-  }
-
   ngOnInit(): void {
-    this.setUpAnalytics();
     console.log("%cWhat are you looking at 👀?!", "color: green");
-  }
-
-  setUpAnalytics() {
-    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
-        .subscribe((event: NavigationEnd) => {
-            gtag('config', 'G-5JENKT7JP8',
-                {
-                    page_path: event.urlAfterRedirects
-                }
-            );
-        });
   }
 
 }
