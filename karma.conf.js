fab3e53debfa481ad98052e3beba1f426264111c
magalenyo <magalenyo7@gmail.com>
2022-03-05 18:37:44 +0100

    Revert "Rename"
    This reverts commit 6c9b6d22d4b1be69795295eaafeecfd34e04dc79.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/magalenyo.github.io'),
+      dir: require('path').join(__dirname, './coverage/CVWebPage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
